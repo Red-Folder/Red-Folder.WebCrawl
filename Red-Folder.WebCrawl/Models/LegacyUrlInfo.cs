d7c0ae1cf19af5dd3ca423aecbd2e1bd9e9a799d
Mark Taylor <mark@red-folder.com>
2016-12-27 20:41:25 +0000

    Added Legacy Processor

---
+++ Red-Folder.WebCrawl/Models/LegacyUrlInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Red_Folder.WebCrawl.Models
+{
+    public class LegacyUrlInfo : BaseUrlInfo
+    {
+        public LegacyUrlInfo(string url)
+        {
+            _url = url;
+        }
+
+        public LegacyUrlInfo(string url, string invalidationMessage)
+        {
+            _url = url;
+            _invalidationMessage = invalidationMessage;
+        }
+    }
+}
